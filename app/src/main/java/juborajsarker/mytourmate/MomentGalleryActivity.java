367a2f33024cd677a52c7e09908d681dab4e3a9e
Juboraj Sarker <juborajsarker001@gmail.com>
2017-02-16 15:45:56 +0600

    Final Commit

--- app/src/main/java/juborajsarker/mytourmate/MomentGalleryActivity.java
+++ app/src/main/java/juborajsarker/mytourmate/MomentGalleryActivity.java
@@ -1,7 +1,11 @@
 package juborajsarker.mytourmate;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MomentGalleryActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MomentGalleryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moment_gallery);
+
+
+        Bundle extras = getIntent().getExtras();
+        byte[] byteArray = extras.getByteArray("picture");
+        Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        ImageView image = (ImageView) findViewById(R.id.showImageView);
+        TextView textView = (TextView) findViewById(R.id.textWillBeShow);
+        String m = getIntent().getStringExtra("moment");
+        textView.setText(m);
+        image.setImageBitmap(bmp);
     }
 }
--- app/src/main/java/juborajsarker/mytourmate/TravelMomentActivity.java
+++ app/src/main/java/juborajsarker/mytourmate/TravelMomentActivity.java
@@ -1,41 +1,49 @@
 package juborajsarker.mytourmate;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 public class TravelMomentActivity extends AppCompatActivity {
 
+    private static final int CAMERA_REQUEST_CODE = 1;
+    EditText editText;
+    ProgressDialog mProgress;
     private Button mUploadBtn;
     private ImageView mImageView;
-    private static final int CAMERA_REQUEST_CODE = 1;
+    private ImageView saveImageShow;
     private StorageReference mStorage;
-    ProgressDialog mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel_moment);
 
+        editText = (EditText) findViewById(R.id.travel_moment);
+
         mStorage = FirebaseStorage.getInstance().getReference();
         mProgress = new ProgressDialog(this);
 
         mUploadBtn = (Button) findViewById(R.id.uploadBtn);
         mImageView = (ImageView) findViewById(R.id.imageCapture);
+        saveImageShow = (ImageView) findViewById(R.id.saveImageShow);
     }
 
     public void viewGalarry(View view) {
@@ -43,42 +51,59 @@ public class TravelMomentActivity extends AppCompatActivity {
         startActivity(new Intent(TravelMomentActivity.this, MomentGalleryActivity.class));
     }
 
-    public void save_moment(View view) {
 
-    }
 
     public void takePhoto(View view) {
 
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+
+        Intent camera = new Intent(
+                android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camera, 1);
+
+
     }
 
-    @Override
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
         if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
-
-            mProgress.setMessage("Uploading...");
-            mProgress.show();
-
-            Uri uri = data.getData();
-            StorageReference filePath = mStorage.child("photos").child(uri.getLastPathSegment());
-            filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                    mProgress.dismiss();
-                    Toast.makeText(TravelMomentActivity.this, "Uploading finished", Toast.LENGTH_SHORT).show();
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-
-                public void onFailure(@NonNull Exception exception) {
-
-                    Toast.makeText(TravelMomentActivity.this, "Somethig went wrong", Toast.LENGTH_SHORT).show();
-                }
-            });
-
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            saveImageShow.setImageBitmap(photo);
+            Uri tempUri = getImageUri(getApplicationContext(), photo);
+            File finalFile = new File(getRealPathFromURI(tempUri));
         }
     }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    public String getRealPathFromURI(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+        return cursor.getString(idx);
+    }
+
+
+    public void save_moment(View view) {
+
+        Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.id.imageCapture);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        //bmp.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+
+
+        Intent intent = new Intent(TravelMomentActivity.this, MomentGalleryActivity.class);
+        intent.putExtra("picture", byteArray);
+        intent.putExtra("moment", editText.getText().toString());
+        startActivity(intent);
+
+    }
+
+
+
 }
+
